d3882f30a82491a2e96cb399711321f4de393a27
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2022-05-12 18:47:38 +0500

    Update db.php

--- lab4/php/db.php
+++ lab4/php/db.php
@@ -1,7 +1,7 @@
 <?
 require "libs/rb.php";
 R::setup(
-	'mysql:host=https://www.phpmyadmin.co/db_structure.php?db=sql6491835;dbname=sql6491835',
+	'mysql:host=sql6.freemysqlhosting.net;dbname=sql6491835',
 	'sql6491835',
 	'1MhAGGbyba'
 );
