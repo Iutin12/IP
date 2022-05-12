125ae0a34f75eb0f195a180904b0eeb6dd7d8f26
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2022-05-12 19:55:31 +0500

    Update connect.php

--- lab3/php/connect.php
+++ lab3/php/connect.php
@@ -1,6 +1,6 @@
 <?php
-$servername = "sql6.freemysqlhosting.net";
-$database = "sql6491835";
+$servername = "localhost";
+$database = "asql6491835";
 $username = "sql6491835";
 $password = "1MhAGGbyba";
 $connect = mysqli_connect($servername, $username, $password, $database);
