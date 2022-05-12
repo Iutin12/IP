f92ca9a9d73a55006a1b0b0372b8a77ece642f37
Iutin12 <79744000+Iutin12@users.noreply.github.com>
2022-05-12 18:43:40 +0500

    Update connect.php

--- lab3/php/connect.php
+++ lab3/php/connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$database = "a0669052_recipe";
-$username = "a0669052_recipe";
-$password = "12345";
+$servername = "sql6.freemysqlhosting.net";
+$database = "sql6491835";
+$username = "sql6491835";
+$password = "1MhAGGbyba";
 $connect = mysqli_connect($servername, $username, $password, $database);
 if (!$connect) {
     die("Connection failed: " . mysqli_connect_error());
